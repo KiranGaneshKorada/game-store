cf4ff5d77c8c179df9b3c4e93a0921ed430c4dbb
Kiran Ganesh Korada <kiranganeshkorada@gmail.com>
2023-10-21 12:04:08 +0530

    navbar made-responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,14 +6,16 @@ import SearchComponent from "./SearchComponent";
 function Navbar() {
   return (
     <>
-      <div className="navbar sticky-Nav bg-secondary bg-opacity-50 my-0 mx-0 py-0 px-0">
-        <Link className="navbar-brand mx-5" to="/games">
-          Game-Store
-        </Link>
-        <div className="col  text-white my-0 mx-0 py-0 px-0 my-1 ">
-          <SearchComponent />
+      <nav className="navbar sticky-Nav navbar-expand-lg bg-secondary bg-opacity-50 my-0 mx-0 py-0 px-0">
+        <div className="d-flex justify-content-center container-fluid pe-0">
+          <Link className=" navbar-brand me-0" to="/games">
+            Game-Store
+          </Link>
+          <div className="col  text-white my-0 mx-0 py-0 px-0 my-1 ">
+            <SearchComponent />
+          </div>
         </div>
-      </div>
+      </nav>
     </>
   );
 }
